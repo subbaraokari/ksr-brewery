4b892fc540d238a0c5bc0ff563bc7996563b7116
ksrao <ksraotech@gmail.com>
2021-09-04 20:38:51 +0530

    project initializes closing #1

---
+++ src/main/java/com/cts/ksrbrewery/KsrBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.cts.ksrbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsrBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsrBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cts/ksrbrewery/KsrBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.ksrbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsrBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
